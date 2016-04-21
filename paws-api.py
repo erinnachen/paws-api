5c85a58f7538c2499b7824e480f2d00ba1dc3691
Erinna Chen <erinna.chen@gmail.com>
2016-04-21 12:53:34 -0600

    Add Flask-CORS to handle requests * Allows for Ajax calls involving Cross Origin Resource Sharing

--- paws-api.py
+++ paws-api.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
 from random import randint
+from flask.ext.cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/api/v1/dog_image_categories")
 def analysis():
