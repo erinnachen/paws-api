5c162dc956b0a8f50f17bd93773dd2f09f845949
Erinna Chen <erinna.chen@gmail.com>
2016-04-22 17:12:18 -0600

    Add basic image analyzer to api

---
+++ image_analyzer.py
@@ -0,0 +1,81 @@
+from keras.models import Sequential
+from keras.layers.core import Flatten, Dense, Dropout
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.optimizers import SGD
+import cv2, numpy as np
+import urllib
+
+def VGG_16(weights_path=None):
+    model = Sequential()
+    model.add(ZeroPadding2D((1,1),input_shape=(3,224,224)))
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(Flatten())
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1000, activation='softmax'))
+
+    if weights_path:
+        model.load_weights(weights_path)
+
+    return model
+
+def analyze_image(image_url):
+    print "HERE IS THE" + image_url
+    urllib.urlretrieve(image_url, "image")
+    print "Load the image"
+    im = cv2.resize(cv2.imread("image"), (224, 224)).astype(np.float32)
+    im[:,:,0] -= 103.939
+    im[:,:,1] -= 116.779
+    im[:,:,2] -= 123.68
+    im = im.transpose((2,0,1))
+    im = np.expand_dims(im, axis=0)
+
+    print "Loading the model"
+    model = VGG_16('vgg16_weights.h5')
+    sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+    print "Compiling the model"
+    model.compile(optimizer=sgd, loss='categorical_crossentropy')
+    print "Testing out the model"
+    out = model.predict(im)
+    print type(out)
+    print out.max()
+    print np.where( out > 0.25 )
+    print np.argmax(out)
+    categories = [line.strip() for line in open("synset_words.txt", 'r')]
+    return categories[np.argmax(out)][10:].split(",")[0]
--- paws-api.py
+++ paws-api.py
@@ -1,14 +1,16 @@
 from flask import Flask, request, jsonify
 from random import randint
 from flask.ext.cors import CORS
+from image_analyzer import analyze_image
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/api/v1/dog_image_categories")
 def analysis():
-    breeds = ["German Shepherd", "Chihuahua", "Boston Terrier", "Siberian Husky", "Labrador Retriever", "Golden Retriever", "Poodle"]
-    return jsonify(breed=breeds[randint(0, len(breeds)-1)])
+    image_url = request.args.get('image')
+    image_breed = analyze_image(image_url)
+    return jsonify(breed=image_breed)
 
 if __name__ == "__main__":
     app.run()
